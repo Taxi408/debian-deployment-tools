4ce66aa7464623ec30325be0970353b77390473c
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 05:37:35 +0200

    Create apt-backup-repo.sh

---
+++ apt-backup-repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+cd /
+apt-dir=/etc/apt
+#apt-backup=/rts/backup/system/apt
+apt-backup=/tests/dirtest
+apt-repo-oslist=${apt-dir}/sources.list
+apt-repo-lists=${apt-dir}/sources.list.d
+apt-repo-trusted=${apt-dir}/trusted.gpg.d
+#
+ls -a -l -G -x -p --color ${apt-repo-lists}
+ls -a -l -G -x -p --color ${apt-repo-trusted}
+#
+cd ${apt-backup}
+mkdir "$(date -R)" && cd "$(date -R)" && cp ${apt-repo-oslist} sources.list && mkdir sources.list.d && mkdir trusted.gpg.d && cp ${apt-repo-lists}/* sources.list.d/ && cp ${apt-repo-trusted}/* trusted.gpg.d/
