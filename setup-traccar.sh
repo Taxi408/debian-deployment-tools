f0d5ef1278b17e9ceb1928414be61cdd5c3c7706
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 06:11:32 +0200

    update setup-traccar.sh

--- setup-traccar.sh
+++ setup-traccar.sh
@@ -2,19 +2,20 @@
 #
 cd /
 #
-dlfile=traccar-linux-64-5.9.zip
-urldlfile=https://github.com/traccar/traccar/releases/download/v5.9/traccar-linux-64-5.9.zip
-runfile=traccar.run
-appversion=5.9
+dlfile='traccar-linux-64-5.9.zip'
+urldlfile='https://github.com/traccar/traccar/releases/download/v5.9/traccar-linux-64-5.9.zip'
+runfile='traccar.run'
+appversion='5.9'
+logfile='/rts/logs/setup-traccar.log'
 #
-mkdir /opt/traccar
-mkdir /rts/software/traccar
+mkdir /opt/traccar 1>> $logfile 2>> $logfile
+mkdir /rts/software/traccar 1>> $logfile 2>> $logfile
 #
 cd /rts/software/traccar
 echo "# TRACCAR Server" >> info.md
 echo " " >> info.md
 echo "Version: "${appversion} >> info.md
-echo "Setup-Time: "$(date) >> info.md
+echo "Setup-Time: "$(date -R) >> info.md
 echo "Source URL: "${urldlfile} >> info.md
 echo "Source File: "${dlfile} >> info.md
 echo " " >> info.md
@@ -28,11 +29,11 @@ echo "| Start Service | systemctl start traccar |" >> info.md
 echo "| Stop Service | systemctl stop traccar |" >> info.md
 echo "| Get Service Status | systemctl status traccar |" >> info.md
 #
-cd /opt/traccar
-wget ${urldlfile}
-unzip ${dlfile}
+cd /opt/traccar 1>> $logfile 2>> $logfile
+wget ${urldlfile} 1>> $logfile 2>> $logfile
+unzip $ 1>> $logfile 2>> $logfile
 #
-bash ./${runfile}
-systemctl enable traccar
-systemctl start traccar
+bash ./${runfile} 1>> $logfile 2>> $logfile
+systemctl enable traccar 1>> $logfile 2>> $logfile
+systemctl start traccar 1>> $logfile 2>> $logfile
 #
