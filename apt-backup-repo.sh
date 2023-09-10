9ef523281f793b4b82cd430e9d70e494d844b5bd
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 06:00:35 +0200

    update apt-backup-repo.sh

--- apt-backup-repo.sh
+++ apt-backup-repo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 cd /
-apt-dir=/etc/apt
-#apt-backup=/rts/backup/system/apt
-apt-backup=/test/dirtest
-apt-repo-oslist=${apt-dir}/sources.list
-apt-repo-lists=${apt-dir}/sources.list.d
-apt-repo-trusted=${apt-dir}/trusted.gpg.d
+apt-dir='/etc/apt'
+#apt-backup='/rts/backup/system/apt'
+apt-backup='/test/dirtest'
+apt-repo-oslist="${apt-dir}/sources.list"
+apt-repo-lists="${apt-dir}/sources.list.d"
+apt-repo-trusted="${apt-dir}/trusted.gpg.d"
 #
-ls -a -l -G -x -p --color ${apt-repo-lists}
-ls -a -l -G -x -p --color ${apt-repo-trusted}
+ls -a -l -G -x -p --color ${apt-repo-lists} >> "${apt-backup}/sources.list.d.log"
+ls -a -l -G -x -p --color ${apt-repo-trusted} >> "${apt-backup}/trusted.gpg.d.log"
 #
 cd ${apt-backup}
 mkdir "$(date -R)" && cd "$(date -R)" && cp ${apt-repo-oslist} sources.list && mkdir sources.list.d && mkdir trusted.gpg.d && cp ${apt-repo-lists}/* sources.list.d/ && cp ${apt-repo-trusted}/* trusted.gpg.d/
