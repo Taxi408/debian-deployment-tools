e98d9e7f75fe0b2e578ea3a080c983d73e0e26d8
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 04:16:23 +0200

    Create setup-basics.sh

---
+++ setup-basics.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+cd /
+#
+logfiledir=/rts/logs
+urldlres=http://h3001362.stratoserver.net/resources
+#
+mkdir /rts
+mkdir /rts/logs
+mkdir /rts/script
+mkdir /rts/sysinfo
+mkdir /rts/sysconf
+mkdir /rts/software
+mkdir /rts/security
+#
+#echo "CURRENT USER: "$USER >> 
+#
+wget ${urldlres}/setup-docker.sh
+wget ${urldlres}/setup-traccar.sh
+wget ${urldlres}/setup-plesk.sh
+wget ${urldlres}/setup-basics.sh
+wget ${urldlres}/updater.sh
+wget ${urldlres}/set-file-attributes.sh
+#
+chmod +x set-file-attributes.sh 1>> $logfiledir/setup-basics.out.sh
+bash ./set-file-attributes.sh 1>> $logfiledir/setup-basics.set-file-attributes.out.sh 2>> $logfiledir/setup-basics.set-file-attributes.err.log
+#
+echo "Installed Software" >> "/rts/sysinfo/installed-software.md"
+echo "Basic Server Info" >> "/rts/sysinfo/server-basic.md"
+echo "Server-Hostname: "$HOSTNAME >> "/rts/sysinfo/server-basic.md"
+echo "Terminal-Emulation: "$TERM >> "/rts/sysinfo/server-basic.md"
+echo "Current Shell-Type: "$SHELL >> "/rts/sysinfo/server-basic.md"
+echo "Command-Paths-List: "$PATH >> "/rts/sysinfo/server-basic.md"
+echo "Current set Editor: "$EDITOR >> "/rts/sysinfo/server-basic.md"
+
+#
+bash /rts/script/updater.sh 1>> $logfiledir/setup-basics.updater.run0001.out.log 2>> $logfiledir/setup-basics.updater.run0001.err.log #9>> $logfiledir/setup-basics.updater.run0001.all.log
+#
+apt-get -y install mc 1>> $logfiledir/setup-mc.out.log 2>> $logfiledir/setup-mc.err.log
+apt-get -y install nano 1>> $logfiledir/setup-nano.out.log 2>> $logfiledir/setup-nano.err.log
+apt-get -y install apache2 1>> $logfiledir/setup-apache2.out.log 2>> $logfiledir/setup-apache2.err.log
+apt-get -y install htop 1>> $logfiledir/setup-htop.out.log 2>> $logfiledir/setup-htop.err.log
+apt-get -y install curl 1>> $logfiledir/setup-curl.out.log 2>> $logfiledir/setup-curl.err.log
+#apt-get -y install apt-transport-https
+#apt-get -y install vsftpd
+#apt-get -y install colorized-logs
+apt-get -y install git
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+#apt-get -y install 
+bash /rts/script/updater.sh 1>> $logfiledir/setup-basics.updater.run0002..out.log 2>> $logfiledir/setup-basics.updater.run0002.err.log #9>> $logfiledir/setup-basics.updater.run0002.all.log
+#
