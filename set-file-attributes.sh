3eb23a588f0fda90d892b442b0e5ec7f32a6a586
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 06:12:37 +0200

    update set-file-attributes.sh

--- set-file-attributes.sh
+++ set-file-attributes.sh
@@ -2,10 +2,11 @@
 #
 cd /
 #
-chmod +x setup-plesk.sh
-chmod +x setup-traccar.sh
-chmod +x setup-docker.sh
-chmod +x setup-basics.sh
-chmod +x updater.sh
-chmod +x setup-git.sh
-chmod +x apt-backup-repo.sh
+chmod +x 'setup-plesk.sh'
+chmod +x 'setup-traccar.sh'
+chmod +x 'setup-docker.sh'
+chmod +x 'setup-basics.sh'
+chmod +x 'updater.sh'
+chmod +x 'setup-git.sh'
+chmod +x 'apt-backup-repo.sh'
+#
