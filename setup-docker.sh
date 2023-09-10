36f21747453a000c4de08c8b8ea2f06e80432fae
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 06:29:38 +0200

    update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 #
 cd /
+logfile='/rts/logs/setup-docker.log'
 #
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
+1>> $logfile 2>> $logfile
