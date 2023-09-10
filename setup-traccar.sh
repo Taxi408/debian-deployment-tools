7c1bda04b6be6c0c0ecf888da253ee2b5572ecf2
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 03:28:59 +0200

    update setup-traccar.sh

--- setup-traccar.sh
+++ setup-traccar.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+#
+cd /
+#
+dlfile=traccar-linux-64-5.9.zip
+urldlfile=https://github.com/traccar/traccar/releases/download/v5.9/traccar-linux-64-5.9.zip
+runfile=traccar.run
+appversion=5.9
+#
+mkdir /opt/traccar
+mkdir /rts/software/traccar
+#
+cd /rts/software/traccar
+echo "# TRACCAR Server" >> info.md
+echo " " >> info.md
+echo "Version: "${appversion} >> info.md
+echo "Setup-Time: "$(date) >> info.md
+echo "Source URL: "${urldlfile} >> info.md
+echo "Source File: "${dlfile} >> info.md
+echo " " >> info.md
+echo "## Help Resources" >> info.md
+echo "Vendor Installation Instructions: https://www.traccar.org/linux/" >> info.md
+echo " " >> info.md
+echo "## OS Control Commands" >> info.md
+echo "| Use | command |" >> info.md
+echo "| :-- | :------ |" >> info.md
+echo "| Start Service | systemctl start traccar |" >> info.md
+echo "| Stop Service | systemctl stop traccar |" >> info.md
+echo "| Get Service Status | systemctl status traccar |" >> info.md
+#
+cd /opt/traccar
+wget ${urldlfile}
+unzip ${dlfile}
+#
+bash ./${runfile}
+systemctl enable traccar
+systemctl start traccar
+#
