9a720ba4119ceab12e974da43aa9d95ddb7c96d5
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 08:30:26 +0200

    update set-file-attributes.sh

--- set-file-attributes.sh
+++ set-file-attributes.sh
@@ -5,9 +5,10 @@ cd /
 chmod +x 'software/management/setup-plesk.sh'
 chmod +x 'software/management/setup-traccar.sh'
 chmod +x 'software/management/setup-docker.sh'
-chmod +x 'software/management/setup-basics.sh'
+chmod +x 'setup-basics.sh'
 chmod +x 'os-tools/updater.sh'
 chmod +x 'software/management/setup-git.sh'
 chmod +x 'os-tools/apt-backup-repo.sh'
 chmod +x 'software/management/setup-mysqlserver.sh'
+chmod +x 'connections/external/dns-update-dynv6.sh'
 #
