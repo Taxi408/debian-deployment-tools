8587e544aafb6d81efd0a5568c16e25c77e62d2a
Taxi408 <125762906+Taxi408@users.noreply.github.com>
2023-09-10 06:26:23 +0200

    update setup-plesk.sh

--- setup-plesk.sh
+++ setup-plesk.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 #
 cd /
+logfile='/rts/logs/setup-plesk.sh'
 #
+./updater.sh 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
+ 1>> $logfile 2>> $logfile
